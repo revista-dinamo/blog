fdc373772f332d9fc93aa260ce41008160817c3d
gonzae <gonzae@virtual2.(none)>
2013-06-29 13:54:09 -0300

    added buscar section. css fixes

--- modules/access.php
+++ modules/access.php
@@ -1,6 +1,6 @@
 <div class="access">
 	<div class="menu">
-		<a class="page_item" href="<?php bloginfo('url'); ?>">Inicio</a>
+		<a class="page_item" style="width:112px;" href="<?php bloginfo('url'); ?>">Inicio</a>
 		<?php foreach (Helper::getMenuElements() as $element) { ?>
 		<a class="page_item" href="<?=$element['link']?>"><?=$element['name']?></a>
 		<?php } ?>
